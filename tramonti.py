b785fdda5176bfe3aaa1f9b1cf767906e1554c51
TizianoMMercuri <tiziano.mercuri04@gmail.com>
2026-02-11 09:53:40 +0100

    Added new info to "tramonti.py"

--- tramonti.py
+++ tramonti.py
@@ -1,9 +1,14 @@
 import funz as f #modulo che contiene le funzioni costruite
 import argparse
+
+
+#definizione del codice argparse per separare le due parti principali del progetto 
+
+
 def argomenti_parse():
     parser = argparse.ArgumentParser(description="Spiegazione comandi.", usage="python3 tramonti.py  --opzione")
-    parser.add_argument("-I", "--Interattivo", action="store_true", help="Esegue in modalità interattiva")
-    parser.add_argument("-O", "--Ozono", action="store_true", help="Consente la visualizzazione sullo studio dell'Ozono")
+    parser.add_argument("-I", "--Interattivo", action="store_true", help="Esegue in modalità interattiva")#aggiungo la prima parte
+    parser.add_argument("-O", "--Ozono", action="store_true", help="Consente la visualizzazione sullo studio dell'Ozono")#aggiungo la seconda parte
     return parser.parse_args()
 def main():
     args=argomenti_parse()
@@ -14,4 +19,4 @@ def main():
     else:
         print("Uscita dal programma...")
 if __name__== "__main__":
-    main()#TEST DA TERMINALE
+    main()
